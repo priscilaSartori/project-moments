f7156673bd916afa854c058e1774286e52b01548
Priscila <priscilasartori2@gmail.com>
2023-07-23 19:16:36 -0300

    style: CSS nos componentes de layout

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NewMomentComponent } from './pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './pages/home/home.component';
     HeaderComponent,
     FooterComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    NewMomentComponent
   ],
   imports: [
     BrowserModule,
