e9b23d9b5e7d7d4fdb2c7fb6ffd3ef5dbdc2c1d1
Priscila <priscilasartori2@gmail.com>
2023-07-25 02:35:20 -0300

    feat: Iniciando componente de mensagem

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NewMomentComponent } from './pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -21,14 +23,15 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AboutComponent,
     HomeComponent,
     NewMomentComponent,
-    MomentFormComponent
+    MomentFormComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule, FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
